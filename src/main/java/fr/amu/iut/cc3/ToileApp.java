0648c5fdd48bb30ac6e1ae5c862a0e2130ae7d94
Hospital Jules <jules.hospital@etu.univ-amu.fr>
2023-06-02 14:04:14 +0200

    ajout de l'arrière plan + resize pour voir les compétences cachés + titre

--- src/main/java/fr/amu/iut/cc3/ToileApp.java
+++ src/main/java/fr/amu/iut/cc3/ToileApp.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,8 +17,14 @@ public class ToileApp extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("toile.fxml"));
         primaryStage.setResizable(false);
-        primaryStage.setScene( new Scene(root) );
+        root.setStyle("-fx-background-color:#bcd0fb;");
+        Scene scene = new Scene(root, 600, 500);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Résultats aux différentes compétences du BUT");
         primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 
 }
